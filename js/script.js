9ef8768bf4dabbe53dcf0d8eaf2d6fb2609afb07
MauricioAcosta <andres.taamap@gmail.com>
2016-10-07 07:05:51 -0500

    pruebas...

--- js/script.js
+++ js/script.js
@@ -1,109 +1,134 @@
-function capturar() {
-    window.alert("Recuerde cargas en  Coulomb \n y Coordenadas en Metros")
-        //carga 1
-    var q1 = document.getElementById('Q1').value
-    var x1 = document.getElementById('X1').value
-    var y1 = document.getElementById('Y1').value
-    var z1 = document.getElementById('Z1').value
+var NumCargas = null;
+var R = null;
+var matriz = null;
 
-    var corq1 = new Array
-    corq1 = [x1, y1, z1]
+//Carga de Prueba
+var x = null;
+var y = null;
+var z = null;
+var corp = new Array
+    /**
+     * Esta funcion se usa para dar los espacios de los coeficientes
+     * ingresando el NumCargas de la ecuacion
+     */
+function armarCondiciones() {
 
-
-    //carga 2
-    var q2 = document.getElementById('Q2').value
-    var x2 = document.getElementById('X2').value
-    var y2 = document.getElementById('Y2').value
-    var z2 = document.getElementById('Z2').value
-
-    var corq2 = new Array
-    corq2 = [x2, y2, z2]
-
-    //arga 3
-    var q3 = document.getElementById('Q3').value
-    var x3 = document.getElementById('X3').value
-    var y3 = document.getElementById('Y3').value
-    var z3 = document.getElementById('Z3').value
-
-    var corq3 = new Array
-    corq3 = [x3, y3, z3]
-
-
-    //cccarga de prueba
-    var x = document.getElementById('X').value
-    var y = document.getElementById('Y').value
-    var z = document.getElementById('Z').value
-
-    var corp = new Array
+    xp = document.getElementById('X').value
+    yp= document.getElementById('Y').value
+    zp = document.getElementById('Z').value
+    x=Number(xp);
+    x=Number(yp);
+    x=Number(zp);
     corp = [x, y, z]
-
-    try {
-        var r1 = Math.sqrt(Math.pow(corq1[0] - corp[0], 2) + Math.pow(corq1[1] - corp[1], 2) + Math.pow(corq1[2] - corp[2], 2))
-//console.log('r1 '+r1);
-        var r2 = Math.sqrt(Math.pow(corq2[0] - corp[0], 2) + Math.pow(corq2[1] - corp[1], 2) + Math.pow(corq2[2] - corp[2], 2))
-//console.log('r1 '+r2);
-        var r3 = Math.sqrt(Math.pow(corq3[0] - corp[0], 2) + Math.pow(corq3[1] - corp[1], 2) + Math.pow(corq3[2] - corp[2], 2))
-//console.log('r1 '+r3);
-        vector1 = EvaluarVector(corq1, corp, r1)
-        vector2 = EvaluarVector(corq2, corp, r2)
-        vector3 = EvaluarVector(corq3, corp, r3)
-
-        campoElectrico1 = EvaluarCampo(q1, vector1, r1)
-        campoElectrico2 = EvaluarCampo(q2, vector2, r2)
-        campoElectrico3 = EvaluarCampo(q3, vector3, r3)
-
-        var muestra1 = document.getElementById('Muestra1')
-        var muestra2 = document.getElementById('Muestra2')
-        var muestra3 = document.getElementById('Muestra3')
-        var muestra4 = document.getElementById('Muestra4')
-
-        muestra1.innerHTML = 'E<sub>T</sub> = E<sub>q1p</sub> + E<sub>q2p</sub> + E<sub>q3p</sub>';
-        muestra2.innerHTML = 'E<sub>T</sub> = ' + campoElectrico1[0] + 'i + ' + campoElectrico1[1] + 'j + ' + campoElectrico1[2] + 'k + ' + campoElectrico2[0] + 'i + ' + campoElectrico2[1] + 'j + ' + campoElectrico2[2] + 'k + ' + campoElectrico3[0] + 'i + ' + campoElectrico3[1] + 'j + ' + campoElectrico3[2] + 'k '
-
-        var i = campoElectrico1[0] + campoElectrico2[0] + campoElectrico3[0]
-        var j = campoElectrico1[1] + campoElectrico3[1] + campoElectrico2[1]
-        var k = campoElectrico1[2] + campoElectrico2[2] + campoElectrico3[2]
-
-        muestra3.innerHTML = 'E<sub>T</sub> = ' + i + ' i + ' + j + ' j + ' + k + ' k '
-
-        normaE = NormaCampo(i, j, k)
-
-        muestra4.innerHTML = '|E| = ' + normaE + ' N/C '
-    }catch (e) {
-
-    window.alert('Ingresaste valores incorrectos')
-
-} finally {
+        //Solicita datos (NumCargas)
+    var temp = document.getElementById("NumCargas").value;
+    NumCargas = Number(temp);
+    matriz = document.getElementById('Matriz')
+    var temp2 = document.getElementById("R").value;
+    R = Number(temp2);
+    //Analiza si se ingreso el NumCargas adecuado
+    if (NumCargas === null || NumCargas === 0) {
+        alert("Ingrese Orden");
+        return;
+    }
+    if (R == 2) {
+        window.alert("Recuerde cargas en  Coulomb \n y Cordenadas en Metros")
+        dibujarMatriz(matriz)
+        armarTablaValoresIniciales()
+    } else if (R == 3) {
+        window.alert("Recuerde cargas en  Coulomb \n y Coordenadas en Metros")
+        dibujarMatriz(matriz)
+        armarTablaValoresIniciales(NumCargas)
+    } else {
+        window.alert('Solo se puede para R^2 y R^3')
+    }
 
 }
 
-
-
+function dibujarMatriz(matriz) {
+    matriz.innerHTML = "";
+    for (i = 0; i < NumCargas; i++) {
+        var nuevaFila = matriz.insertRow(-1)
+        for (j = 0; j < R; j++) {
+            var CELDA = nuevaFila.insertCell(-1);
+            //  CELDA += ' Q_' + (j) + ': '
+            var input = document.createElement('input')
+            input.setAttribute('type', 'text')
+            input.setAttribute('data-fila', i)
+            input.setAttribute('data-columna', j)
+            input.setAttribute('size', '3')
+            CELDA.appendChild(input)
+        }
+    }
 }
 
-function EvaluarVector(corq, corp, r) {
-    var rvector = new Array
-    rvector[0] = ((corq[0] - corp[0]) / r)*(-1)
-    rvector[1] = ((corq[1] - corp[1]) / r)*(-1)
-    rvector[2] = ((corq[2] - corp[2]) / r)*(-1)
-    console.log('EvaluarVector '+rvector);
+function armarTablaValoresIniciales(NumCargas) {
+  //Crea los espacios para los coeficientes
+   var t = document.getElementById("tablaDeCargas");
+   t.innerHTML = ''
+   for (i = 0; i < NumCargas.length; i++) {
+       var f = t.insertRow(-1);
+       var z = f.insertCell(-1);
+       var CELDA = ' Q_' + (i) + ': '
+       CELDA += '<input type="text"'
+       CELDA += 'data-columna="' + i + '" ';
+       CELDA += 'size="3">'
+
+       z.innerHTML = CELDA;
+   }
+}
+
+function capturar() {
+  var tablaDeCargas = document.getElementById("tablaDeCargas")
+var r = new Array
+    for (var i = 0; i < matriz.length; i++) {
+
+        for (var j = 0; j < 3; j++) {
+            r[i][j] = Math.sqrt(Math.pow(matriz[i][j] - corp[i][j], 2) + Math.pow(matriz[i][j + 1] - corp[i][j + 1], 2) + Math.pow(corq1[i][j + 1] - corp[i][j + 1], 2))
+            j += 2
+        }
+    }
+    var vector = new Array
+    vector = EvaluarVector(matriz, corp, r)
+    campoElectrico = EvaluarCampo(tablaDeCargas, vector, r)
+    NormaCampo = NormaCampo(campoElectrico)
+}
+
+function EvaluarVector(matriz, corp, r) {
+
+  var rvector = new Array
+  for (var i = 0; i < matriz.length; i++) {
+
+      for (var j = 0; j < 3; j++) {
+
+        rvector[i][j]=((matriz[i][j] - corp[i][j]) / r[i][j]) * (-1)
+        j+=2
+      }
+  }
     return rvector
 
 }
 
-function EvaluarCampo(q, vector, r) {
+function EvaluarCampo(tablaDeCargas, vector, r) {
     var k = 9 * Math.pow(10, 9)
     var campoElectrico = new Array
-    campoElectrico[0] = ((k * q) / Math.pow(r,2)) * vector[0]
-    campoElectrico[1] = ((k * q) / Math.pow(r,2)) * vector[1]
-    campoElectrico[2] = ((k * q) / Math.pow(r,2)) * vector[2]
+    for (var i = 0; i < matriz.length; i++) {
+
+        for (var j = 0; j < 3; j++) {
+
+          campoElectrico[i][j]=(k*tablaDeCargas[j]/ Math.pow(r[i][j], 2))*vector[i][j]
+          j+=2
+        }
+    }
     console.log('campoElectrico ' + campoElectrico);
     return campoElectrico
 }
 
-function NormaCampo(i, j, k) {
+function NormaCampo(campoElectrico) {
 
-    var normaE = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2) + Math.pow(k, 2))
+    normaE = Math.sqrt(Math.pow(campoElectrico[0], 2) + Math.pow(campoElectrico[1], 2) + Math.pow(campoElectrico[2], 2))
+
+console.log(normaE);
 
     return normaE
 
