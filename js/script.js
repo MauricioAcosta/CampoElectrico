7cc06cd08d9a3e1b786bdc8e73c48ebd5b075485
alejandro Hernandez <usercall.hm@gmail.com>
2016-10-09 01:19:04 -0500

    modifica logica
    Crea dinamicamnye id con inputs dentro divs

--- js/script.js
+++ js/script.js
@@ -1,40 +1,14 @@
 var NumCargas = null;
 var R = null;
-var matriz = null;
 
 //Carga de Prueba
 var x = null;
 var y = null;
 var z = null;
-var corp = new Array
-    /**
-     * Esta funcion se usa para dar los espacios de los coeficientes
-     * ingresando el NumCargas de la ecuacion
-     */
+
+
 function armarCondiciones() {
 
-    xp = document.getElementById('X').value
-    yp = document.getElementById('Y').value
-    zp = document.getElementById('Z').value
-
-    /*Toamdo el datos de caraga de pruebaa*/
-    x = Number(xp);
-    y = Number(yp);
-    z = Number(zp);
-
-    corp = [
-            [x, y, z],
-            [x, y, z],
-            [x, y, z]
-        ]
-        //Solicita datos (NumCargas)
-
-
-    var temp = document.getElementById("NumCargas").value;
-    NumCargas = Number(temp);
-
-    // donde se va a introducir valores
-    matriz = document.getElementById('Matriz')
 
     //Analiza si se ingreso el NumCargas adecuado
     if (NumCargas === null || NumCargas === 0) {
@@ -46,16 +20,18 @@ function armarCondiciones() {
     if (document.getElementById("R1").checked) {
         R = 1;
         window.alert("Recuerde cargas en  Coulomb \n y Cordenadas en Metros");
-        dibujarMatriz(matriz);
+        //dibujarMatriz(matriz);
+        aniadirCarga();
+
 
     } else if (document.getElementById("R2").checked) {
         R = 2;
         window.alert("Recuerde cargas en  Coulomb \n y Cordenadas en Metros");
-        dibujarMatriz(matriz);
+        //  dibujarMatriz(matriz);
     } else if (document.getElementById("R3").checked) {
         R = 3;
         window.alert("Recuerde cargas en  Coulomb \n y Coordenadas en Metros");
-        dibujarMatriz(matriz);
+
     } else {
         window.alert('Seleciona el campo a trabajar');
     }
@@ -64,83 +40,102 @@ function armarCondiciones() {
 
 }
 
-/*Añade elemanrto para r2 0 r3*/
-function dibujarMatriz(matriz) {
-    /*Numero de cargas*/
-    matriz.innerHTML = "";
+
+// Indexador de cargas
+var iCarga = 0;
+//arreglo donde añado los inputs
+var div = [];
+
+function aniadirCarga() {
+
+    div[iCarga] = document.createElement("div");
+    var d = div[iCarga];
+
+    // Valor de la carga
+    var inputV = document.createElement("input");
+    inputV.setAttribute("type", "number");
+    ////opecional para desavilitar espacio vectorial
+    //inputV.setAttribute("value", "0");
+    inputV.setAttribute("placeholder", "Valor de la carga");
 
 
-    for (i = 0; i < NumCargas; i++) {
-        var nuevaFila = matriz.insertRow(0)
+    inputV.id = "cargaV" + iCarga;
+    d.appendChild(inputV);
 
-        /*Descomposicon vectoriañ*/
-        for (j = 0; j < R; j++) {
-            var CELDA = nuevaFila.insertCell(0);
-            var input = document.createElement('input')
-            input.setAttribute('type', 'text')
-            input.setAttribute('data-fila', i)
-            input.setAttribute('data-columna', j)
-            input.setAttribute('size', '3')
-            CELDA.appendChild(input)
-        }
-    }
+
+    var inputVE = document.createElement("input");
+    inputVE.setAttribute("type", "number");
+    inputVE.id = "cargaVE" + iCarga;
+    inputVE.setAttribute("placeholder", "Potencia de la carga");
+    d.appendChild(inputVE);
+
+    //posicion
+    var inputX = document.createElement("input");
+    inputX.setAttribute("type", "number");
+    inputX.id = "cargaX" + iCarga;
+    d.appendChild(inputX);;
+
+    var inputY = document.createElement("input");
+    inputY.setAttribute("type", "number");
+    inputY.id = "cargaY" + iCarga;
+    d.appendChild(inputY);
+
+
+    var inputZ = document.createElement("input");
+    inputZ.setAttribute("type", "number");
+    inputZ.id = "cargaZ" + iCarga;
+    d.appendChild(inputZ);
+    document.body.appendChild(d);
+    iCarga++;
 }
 
-///////// De aqui para adelante no entiendo ni pite
 
-function capturar() {
-    var tablaDeCargas = document.getElementById("tablaDeCargas")
-    var r = new Array
-    for (var i = 0; i < matriz.length; i++) {
+//////////////////////Calcula campo
 
-        for (var j = 0; j < 3; j++) {
-            r[i][j] = Math.sqrt(Math.pow(matriz[i][j] - corp[i][j], 2) + Math.pow(matriz[i][j + 1] - corp[i][j + 1], 2) + Math.pow(corq1[i][j + 1] - corp[i][j + 1], 2))
-            j += 2
-        }
+function calcularCampo() {
+
+    var valorCarga = [];
+    var valorCargaE = [];
+
+    var r = [];
+    //mE hace refencia a k*q1)/r
+    var mE = [];
+
+    var posX = [];
+    var posY = [];
+    var posZ = [];
+    var K = 9 * Math.pow(10, 9);
+    var componentes = [[]];
+
+    console.log("calculando campo");
+    xp = document.getElementById('X').value
+    yp = document.getElementById('Y').value
+    zp = document.getElementById('Z').value
+
+    // Calculo la normal ->r
+    for (var i = 0; i < iCarga; i++) {
+        posX[i] = document.getElementById("cargaX" + i).value;
+        posY[i] = document.getElementById("cargaY" + i).value;
+        posZ[i] = document.getElementById("cargaZ" + i).value;
+
+        var dx =posX[i] - xp;
+        var dy =posY[i] - yp;
+        var dz = posZ[i] - zp;
+        r[i] = Math.sqrt((Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2)));
+
+        valorCarga[i] =document.getElementById("cargaV"+i ).value;
+        valorCargaE[i] = document.getElementById("cargaVE"+i).value;
+        //var temp = Math.pow(valorCarga[i],valorCargaE[i]);
+        var temp =valorCarga[i]*Math.pow(10,valorCargaE);
+
+        var aux = (K*temp)/r[i];
+
+        componentes[i][0] = aux*(dx/r[i]);
+        componentes[i][1] = aux*(dy/r[i]);
+        componentes[i][2] = aux*(dz/r[i]);
+
+
     }
 
-    var vector = new Array
-    vector = EvaluarVector(matriz, corp, r)
-    campoElectrico = EvaluarCampo(tablaDeCargas, vector, r)
-    NormaCampo = NormaCampo(campoElectrico)
-}
-
-function EvaluarVector(matriz, corp, r) {
-
-    var rvector = new Array
-    for (var i = 0; i < matriz.length; i++) {
-
-        for (var j = 0; j < 3; j++) {
-
-            rvector[i][j] = ((matriz[i][j] - corp[i]) / r[i][j]) * (-1)
-            j += 2
-        }
-    }
-    return rvector
-
-}
-
-function EvaluarCampo(tablaDeCargas, vector, r) {
-    var k = 9 * Math.pow(10, 9)
-    var campoElectrico = new Array
-    for (var i = 0; i < matriz.length; i++) {
-
-        for (var j = 0; j < 3; j++) {
-
-            campoElectrico[i][j] = (k * tablaDeCargas[j] / Math.pow(r[i][j], 2)) * vector[i][j]
-            j += 2
-        }
-    }
-    console.log('campoElectrico ' + campoElectrico);
-    return campoElectrico
-}
-
-function NormaCampo(campoElectrico) {
-
-    normaE = Math.sqrt(Math.pow(campoElectrico[0], 2) + Math.pow(campoElectrico[1], 2) + Math.pow(campoElectrico[2], 2))
-
-    console.log(normaE);
-
-    return normaE
 
 }
